be1fb8d7ec46b98cfc19f2b3ce09e91889b011a7
Manuel <manuel.vanek@gmail.com>
2023-02-26 20:57:40 +0100

    added queries, post and delete

--- backend/src/routes/events.ts
+++ backend/src/routes/events.ts
@@ -1,55 +1,100 @@
-import express from 'express'
-import fs from 'fs'
-
+import express from "express";
+import fs from "fs";
+import bodyParser from "body-parser";
 const router = express.Router();
 
+router.use(bodyParser.json());
+
 router.get("/", (req, res) => {
-    const events =  JSON.parse(fs.readFileSync("./src/data/event.json", "utf-8"));
+  let events = JSON.parse(fs.readFileSync("./src/data/event.json", "utf-8"));
 
-    const query = req.query.from;
+  const fromQuery: any = req.query["from"];
+  const dateFrom = createDateToCompare(fromQuery);
+  const toQuery: any = req.query["to"];
+  const dateTo = createDateToCompare(toQuery);
 
-    if(query === req.query["from"]) {
-        events.map(events.filter((event:any) => {
-            if(event.date.month<10) {
-                const date = `${event.date.year.toString()}-${event.date.month.toString().padStart(2, '0')}-${event.date.day.toString()}`
-                
-            }
-           
-        }))
+  if (req.query["from"] !== undefined && req.query["to"] === undefined) {
+    if (req.query["from"] !== "now") {
+      events = events.filter(
+        (event: any) =>
+          new Date(event.date.year, event.date.month - 1, event.date.day + 1) <=
+          dateFrom
+      );
+    } else {
+      events = events.filter(
+        (event: any) =>
+          new Date(event.date.year, event.date.month - 1, event.date.day + 1) >
+          new Date()
+      );
     }
+  }
 
-   
+  if (req.query["from"] !== undefined && req.query["to"] !== undefined) {
+    if (req.query["from"] !== "now") {
+      events = events.filter(
+        (event: any) =>
+          new Date(event.date.year, event.date.month - 1, event.date.day + 1) >
+            dateFrom &&
+          new Date(event.date.year, event.date.month - 1, event.date.day + 1) <
+            dateTo
+      );
+    } else {
+      events = events.filter(
+        (event: any) =>
+          new Date(event.date.year, event.date.month - 1, event.date.day + 1) >
+            new Date() &&
+          new Date(event.date.year, event.date.month - 1, event.date.day + 1) <
+            dateTo
+      );
+    }
+  }
 
-    res.json(events)
-})
+  // /api/events?category=work&from=2023-03-01
+
+  res.json(events);
+});
+
+const createDateToCompare = (query: string) => {
+  const year: any = query.substring(0, 4);
+  const month: any = query.split("-")[1];
+  const day: any = query.substring(query.lastIndexOf("-") + 1, query.length);
+  return new Date(year, parseInt(month) - 1, parseInt(day) + 1);
+};
 
 router.get("/:id", (req, res) => {
-    const events =  JSON.parse(fs.readFileSync("./src/data/event.json", "utf-8"));
-    const event = events.find((e:any) => e.id.toString() === req.params.id)
-    res.json(event)
-})
+  const events = JSON.parse(fs.readFileSync("./src/data/event.json", "utf-8"));
+  const event = events.find((e: any) => e.id.toString() === req.params.id);
+  res.json(event);
+});
 
 router.post("/", (req, res) => {
- 
-})
+  const event = req.body;
+  let events = JSON.parse(fs.readFileSync("./src/data/event.json", "utf-8"));
+  const url = "./src/data/event.json";
+  console.log(events);
+  events.push(event);
+  fs.writeFile(url, JSON.stringify(events), (e) => {
+    console.error(e);
+  });
 
-router.put("/:id", (req, res) => {
- 
-})
+  res.send("done");
+});
 
-router.patch("/:id", (req, res) => {
- 
-})
+router.put("/:id", (req, res) => {});
 
+router.patch("/:id", (req, res) => {});
 
 router.delete("/:id", (req, res) => {
- 
-})
-/*
+  const id = req.params.id;
+  console.log(id)
+  let events = JSON.parse(fs.readFileSync("./src/data/event.json", "utf-8"));
+  const url = "./src/data/event.json";
+  events = events.filter((e: any) => e.id.toString() !== id);
+  fs.writeFile(url, JSON.stringify(events), (e) => {
 
-/api/events?from=2023-03-01 (only events on or later than this date)
-/api/events?from=now (only future events)
-/api/events?from=2023-03-01&to=2023-03-31
-/api/events?category=work&from=2023-03-01
-*/
-export default router
+  });
+
+  res.send("done");
+});
+
+export default router;
