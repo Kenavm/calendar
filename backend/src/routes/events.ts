2d32833248d2a2a1b9844b1bd12bec171316636b
Manuel <manuel.vanek@gmail.com>
2023-02-27 10:16:52 +0100

    fixed types

--- backend/src/routes/events.ts
+++ backend/src/routes/events.ts
@@ -5,6 +5,19 @@ const router = express.Router();
 
 router.use(bodyParser.json());
 
+type Event = {
+  id: number;
+  name: string;
+  date: {
+    year: number;
+    month: number;
+    day: number;
+    hour: number;
+    minute: number;
+  };
+  category: number;
+};
+
 router.get("/", (req, res) => {
   let events = JSON.parse(fs.readFileSync("./src/data/event.json", "utf-8"));
 
@@ -13,17 +26,17 @@ router.get("/", (req, res) => {
     req.query["to"] === undefined &&
     req.query["category"] === undefined
   ) {
-    const fromQuery: any = req.query["from"];
-    const dateFrom = createDateToCompare(fromQuery);
+    const fromQuery: string = req.query["from"] as string;
+    const dateFrom: Date = createDateToCompare(fromQuery) as Date;
     if (req.query["from"] !== "now") {
       events = events.filter(
-        (event: any) =>
-          new Date(event.date.year, event.date.month - 1, event.date.day + 1) <=
+        (event: Event) =>
+          new Date(event.date.year, event.date.month - 1, event.date.day + 1) >=
           dateFrom
       );
     } else {
       events = events.filter(
-        (event: any) =>
+        (event: Event) =>
           new Date(event.date.year, event.date.month - 1, event.date.day + 1) >
           new Date()
       );
@@ -31,13 +44,13 @@ router.get("/", (req, res) => {
   }
 
   if (req.query["from"] !== undefined && req.query["to"] !== undefined) {
-    const fromQuery: any = req.query["from"];
+    const fromQuery: string = req.query["from"] as string;
     const dateFrom = createDateToCompare(fromQuery);
-    const toQuery: any = req.query["to"];
+    const toQuery: string = req.query["to"] as string;
     const dateTo = createDateToCompare(toQuery);
     if (req.query["from"] !== "now") {
       events = events.filter(
-        (event: any) =>
+        (event: Event) =>
           new Date(event.date.year, event.date.month - 1, event.date.day + 1) >
             dateFrom &&
           new Date(event.date.year, event.date.month - 1, event.date.day + 1) <
@@ -45,7 +58,7 @@ router.get("/", (req, res) => {
       );
     } else {
       events = events.filter(
-        (event: any) =>
+        (event: Event) =>
           new Date(event.date.year, event.date.month - 1, event.date.day + 1) >
             new Date() &&
           new Date(event.date.year, event.date.month - 1, event.date.day + 1) <
@@ -54,27 +67,27 @@ router.get("/", (req, res) => {
     }
   }
   if (req.query["category"] !== undefined && req.query["from"] !== undefined) {
-    const fromQuery: any = req.query["from"];
-    const dateFrom = createDateToCompare(fromQuery);
-    console.log(dateFrom);
-    let categoryQuery = req.query["category"];
+    const fromQuery: string = req.query["from"] as string;
+    const dateFrom: Date = createDateToCompare(fromQuery) as Date;
+
+    let categoryQuery: string = req.query["category"] as string;
     if (categoryQuery === "work") {
       events = events.filter(
-        (event: any) =>
+        (event: Event) =>
           event.category === 2 &&
           new Date(event.date.year, event.date.month - 1, event.date.day + 1) >=
             dateFrom
       );
     } else if (categoryQuery === "personal") {
       events = events.filter(
-        (event: any) =>
+        (event: Event) =>
           event.category === 3 &&
           new Date(event.date.year, event.date.month - 1, event.date.day + 1) >=
             dateFrom
       );
     } else {
       events = events.filter(
-        (event: any) =>
+        (event: Event) =>
           event.category === 1 &&
           new Date(event.date.year, event.date.month - 1, event.date.day + 1) >=
             dateFrom
@@ -86,24 +99,28 @@ router.get("/", (req, res) => {
 });
 
 const createDateToCompare = (query: string) => {
-  const year: any = query.substring(0, 4);
-  const month: any = query.split("-")[1];
-  const day: any = query.substring(query.lastIndexOf("-") + 1, query.length);
-  return new Date(year, parseInt(month) - 1, parseInt(day) + 1);
+  const year: string = query.substring(0, 4);
+  const month: string = query.split("-")[1];
+  const day: string = query.substring(query.lastIndexOf("-") + 1, query.length);
+  return new Date(parseInt(year), parseInt(month) - 1, parseInt(day) + 1);
 };
 
 router.get("/:id", (req, res) => {
   const events = JSON.parse(fs.readFileSync("./src/data/event.json", "utf-8"));
-  const event = events.find((e: any) => e.id.toString() === req.params.id);
+  const event = events.find(
+    (event: Event) => event.id.toString() === req.params.id
+  );
   res.json(event);
 });
 
 router.post("/", (req, res) => {
-  const event = req.body;
-  const url = "./src/data/event.json";
-  let events = JSON.parse(fs.readFileSync("./src/data/event.json", "utf-8"));
+  const event: Event = req.body;
+  const url: string = "./src/data/event.json";
+  let events: Array<Event> = JSON.parse(
+    fs.readFileSync("./src/data/event.json", "utf-8")
+  );
 
-  events.push(event);
+  events = [...events, event];
   fs.writeFile(url, JSON.stringify(events), (e) => {
     console.error(e);
   });
@@ -112,13 +129,15 @@ router.post("/", (req, res) => {
 });
 
 router.put("/:id", (req, res) => {
-  const newEvent = req.body;
-  const id = parseInt(req.params.id);
-  const url = "./src/data/event.json";
-  let events = JSON.parse(fs.readFileSync("./src/data/event.json", "utf-8"));
+  const newEvent: Event = req.body;
+  const id: number = parseInt(req.params.id);
+  const url: string = "./src/data/event.json";
+  let events: Array<Event> = JSON.parse(
+    fs.readFileSync("./src/data/event.json", "utf-8")
+  );
 
-  events = events.map((e: any) => {
-    return e.id === id ? newEvent : e;
+  events = events.map((event: Event) => {
+    return event.id === id ? newEvent : event;
   });
   fs.writeFile(url, JSON.stringify(events), (e) => {
     console.error(e);
@@ -127,10 +146,12 @@ router.put("/:id", (req, res) => {
 });
 
 router.patch("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const newData = req.body;
-  const url = "./src/data/event.json";
-  let events = JSON.parse(fs.readFileSync("./src/data/event.json", "utf-8"));
+  const newData: Event = req.body;
+  const id: number = parseInt(req.params.id);
+  const url: string = "./src/data/event.json";
+  let events: Array<Event> = JSON.parse(
+    fs.readFileSync("./src/data/event.json", "utf-8")
+  );
 
   events = events.map((oldEvent: any) => {
     return oldEvent.id === id ? { ...oldEvent, ...newData } : oldEvent;
@@ -142,10 +163,12 @@ router.patch("/:id", (req, res) => {
 });
 
 router.delete("/:id", (req, res) => {
-  const id = req.params.id;
-  let events = JSON.parse(fs.readFileSync("./src/data/event.json", "utf-8"));
-  const url = "./src/data/event.json";
-  events = events.filter((e: any) => e.id.toString() !== id);
+  const id: number = parseInt(req.params.id);
+  let events: Array<Event> = JSON.parse(
+    fs.readFileSync("./src/data/event.json", "utf-8")
+  );
+  const url: string = "./src/data/event.json";
+  events = events.filter((event: Event) => event.id !== id);
   fs.writeFile(url, JSON.stringify(events), (e) => {});
 
   res.send("done");
